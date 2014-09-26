f90893b42e32b7ef9908bfbf5d93012d1c7a2a2d
CamiloMM <herzschaltung@yahoo.com.ar>
2014-09-25 23:54:33 -0300

    A huge lot of plugin functionality and tests to back it up.

--- code/plugin-system.js
+++ code/plugin-system.js
@@ -1,3 +1,4 @@
+var utils = require('./utils');
 
 // A system for adding "native avisynth functionality" to script objects.
 // In other words, calls to avisynth functions from avisynth.Script()s.
@@ -10,11 +11,58 @@ exports.plugins = {};
 exports.pluginPrototype = {};
 
 // Adds a plugin. Do not ever add a plugin directly and always use this.
-exports.addPlugin = function(name, code) {
+exports.addPlugin = function(name, options, code) {
+    if (!code) { code = options; options = {}; }
+
+    // Do not add a plugin twice, and avoid name collisions.
+    var lowercase = name.toLowerCase();
+    if (exports.plugins[lowercase]) throw new Error('plugin already exists!');
+
+    // Some names are reserved, to avoid overriding script instance methods.
+    var reserved = ['code', 'references', 'load', 'autoload', 'allReferences', 'fullCode'];
+    reserved.forEach(function(res) {
+        if (res.toLowerCase() === lowercase) throw new Error('reserved name!');
+    });
+
+    // Ensure that load and autoload are arrays ('string' -> ['string'], null -> []).
+    var o;
+    options.load = typeof (o = options.load) === 'string' ? [o] : o || [];
+    options.autoload = typeof (o = options.autoload) === 'string' ? [o] : o || [];
+
     var plugin = {};
     plugin.code = code;
+    plugin.options = options;
     plugin.aliases = [];
+
+    // Generate aliases if needed.
     if (/^[A-Z].*[A-Z]/.test(name)) plugin.aliases.push(name[0].toLowerCase() + name.substr(1));
     if (/^.*[A-Z]/.test(name)) plugin.aliases.push(name);
-    exports.plugins[name.toLowerCase()] = plugin;
+
+    exports.plugins[lowercase] = plugin;
+
+    addToPrototype(lowercase, plugin);
 };
+
+// Helper to add a plugin to the prototype.
+function addToPrototype(name, plugin) {
+    var aliases = [name].concat(plugin.aliases);
+
+    // The function added as a plugin wraps the original function, providing
+    // loading, autoloading and (TODO).
+    var code = function() {
+        var context = this;
+        plugin.options.load.forEach(function(item) { context.load(item); });
+        plugin.options.autoload.forEach(function(item) { context.autoload(item); });
+        var returned = plugin.code.apply(context, arguments);
+        if (typeof returned === 'string') {
+            // Add a newline to avoid breaking stuff, if it's not already there.
+            if (this.code[this.code.length - 1] !== '\n') this.code += '\n';
+            this.code += returned;
+        }
+    };
+
+    // The code above will exist for all aliases.
+    aliases.forEach(function(alias) {
+        exports.pluginPrototype[alias] = code;
+    });
+}
--- test/plugins.js
+++ test/plugins.js
@@ -1,9 +1,17 @@
+var path         = require('path');
 var expect       = require('chai').expect;
 var should       = require('chai').should();
 var avisynth     = require('../main');
 var pluginSystem = require('../code/plugin-system');
 
 describe('Plugin system', function() {
+    var fakePluginsDir = path.resolve(__dirname, 'plugins');
+    var scriptPath = path.resolve(fakePluginsDir, 'colors_rgb.avsi');
+    var pluginPath = path.resolve(fakePluginsDir, 'DeDup.dll');
+    var textFile   = path.resolve(fakePluginsDir, 'colors_rgb.txt');
+    var missing    = path.resolve(fakePluginsDir, 'non-existent.dll');
+    var rand = Math.random(); // Guess what, initializing it takes ~260ms for me on Win7.
+
     it('should be exposed as an avisynth.addPlugin reference', function() {
         avisynth.addPlugin.should.equal(pluginSystem.addPlugin);
     });
@@ -11,19 +19,121 @@ describe('Plugin system', function() {
     it('should allow adding plugins and getting them later', function() {
         avisynth.addPlugin('foo', function() {});
         expect(pluginSystem.plugins['foo']).to.exist;
+        pluginSystem.plugins['foo'].code.should.be.a.function;
     });
 
-    it('should automatically lowercase plugin names, and generate aliases', function() {
-        avisynth.addPlugin('FooBar', function() {});
-        pluginSystem.plugins['foobar'].aliases.should.deep.equal(['fooBar', 'FooBar']);
+    it('should allow plugins to be defined with options', function() {
+        var script = new avisynth.Script;
+        avisynth.addPlugin('HaveOptions', {}, function() {});
+        script.haveOptions.should.be.a.function;
     });
 
-    it('should not generate unnecessary aliases', function() {
-        avisynth.addPlugin('pluginOne', function() {});
-        pluginSystem.plugins['pluginone'].aliases.should.deep.equal(['pluginOne']);
-        avisynth.addPlugin('Plugintwo', function() {});
-        pluginSystem.plugins['plugintwo'].aliases.should.deep.equal(['Plugintwo']);
-        avisynth.addPlugin('pluginthree', function() {});
-        pluginSystem.plugins['pluginthree'].aliases.should.deep.equal([]);
+    describe('naming and aliases', function() {
+        it('should throw an error if adding a plugin with a name collision', function() {
+            avisynth.addPlugin('Bar', function() {});
+            avisynth.addPlugin.bind(avisynth, 'baR', function() {}).should.throw();
+        });
+
+        it('should throw an error if adding a plugin with a reserved name', function() {
+            avisynth.addPlugin.bind(avisynth, 'code', function() {}).should.throw();
+            avisynth.addPlugin.bind(avisynth, 'references', function() {}).should.throw();
+            avisynth.addPlugin.bind(avisynth, 'load', function() {}).should.throw();
+            avisynth.addPlugin.bind(avisynth, 'autoload', function() {}).should.throw();
+            avisynth.addPlugin.bind(avisynth, 'allReferences', function() {}).should.throw();
+            avisynth.addPlugin.bind(avisynth, 'fullCode', function() {}).should.throw();
+        });
+
+        it('should automatically lowercase plugin names, and generate aliases', function() {
+            avisynth.addPlugin('FooBar', function() {});
+            pluginSystem.plugins['foobar'].aliases.should.deep.equal(['fooBar', 'FooBar']);
+        });
+
+        it('should not generate unnecessary aliases', function() {
+            avisynth.addPlugin('pluginOne', function() {});
+            pluginSystem.plugins['pluginone'].aliases.should.deep.equal(['pluginOne']);
+            avisynth.addPlugin('Plugintwo', function() {});
+            pluginSystem.plugins['plugintwo'].aliases.should.deep.equal(['Plugintwo']);
+            avisynth.addPlugin('pluginthree', function() {});
+            pluginSystem.plugins['pluginthree'].aliases.should.deep.equal([]);
+        });
+
+        it('should make plugins available as (aliased) Script methods', function() {
+            var script = new avisynth.Script;
+            avisynth.addPlugin('pluginfour' , function() {});
+            avisynth.addPlugin('pluginFive' , function() {});
+            avisynth.addPlugin('Pluginsix'  , function() {});
+            avisynth.addPlugin('PluginSeven', function() {});
+            script.pluginfour.should.be.a.function;
+            script.pluginfive.should.equal(script.pluginFive);
+            script.pluginsix.should.equal(script.Pluginsix);
+            script.pluginseven.should.be.a.function;
+            script.pluginSeven.should.be.a.function;
+            script.PluginSeven.should.be.a.function;
+        });
+    });
+
+    describe('loading and autoloading', function() {
+        it('should allow plugins to be exposed with "load" requirements', function() {
+            var script = new avisynth.Script;
+            avisynth.addPlugin('RequireStuff', {
+                load: [scriptPath, pluginPath]
+            }, function() {});
+            script.requireStuff();
+            var code = script.fullCode().split('\n');
+            code.slice(-3)[0].should.equal('Import("' + scriptPath + '")');
+            code.slice(-2)[0].should.equal('LoadPlugin("' + pluginPath + '")');
+        });
+
+        it('should allow plugins to be exposed with "autoload" requirements', function() {
+            var script = new avisynth.Script;
+            avisynth.addPlugin('RequireDir', {
+                autoload: [fakePluginsDir]
+            }, function() {});
+            script.requireDir();
+            var code = script.fullCode().split('\n');
+            code.slice(-3)[0].should.equal('Import("' + scriptPath + '")');
+            code.slice(-2)[0].should.equal('LoadPlugin("' + pluginPath + '")');
+        });
+
+        it('should allow "load" and "autoload" to be autocasted to array', function() {
+            var script1 = new avisynth.Script;
+            avisynth.addPlugin('RequireStuff2', {
+                load: scriptPath
+            }, function() {});
+            script1.requireStuff2();
+            var code = script1.fullCode().split('\n');
+            code.slice(-2)[0].should.equal('Import("' + scriptPath + '")');
+            code.slice(-1)[0].should.equal('');
+
+            var script2 = new avisynth.Script;
+            avisynth.addPlugin('RequireDir2', {
+                autoload: fakePluginsDir
+            }, function() {});
+            script2.requireDir2();
+            var code = script2.fullCode().split('\n');
+            code.slice(-3)[0].should.equal('Import("' + scriptPath + '")');
+            code.slice(-2)[0].should.equal('LoadPlugin("' + pluginPath + '")');
+            code.slice(-1)[0].should.equal('');
+        });
+    });
+
+    describe('execution facilities', function() {
+        it('should run plugin code when an alias is invoked', function() {
+            var script = new avisynth.Script;
+            var foo = 'bar'
+            avisynth.addPlugin('ClosureTest', function() { foo = 'baz'; });
+            script.closureTest();
+            foo.should.equal('baz');
+        });
+
+        it('should insert code returned from plugin into script', function() {
+            var script = new avisynth.Script;
+            var code = 'Subtitle("' + rand + '")';
+            avisynth.addPlugin('RandomSub', function() { return code; });
+            script.randomsub();
+            var lines = script.fullCode().split('\n');
+            lines.slice(-2)[0].should.equal(code);
+            lines.slice(-1)[0].should.equal('');
+        });
     });
 });
