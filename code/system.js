24ab0f95b8cc991d2e26ffe10463da3150dac194
CamiloMM <herzschaltung@yahoo.com.ar>
2014-12-10 23:58:54 -0200

    Various methods added to system.js.

--- code/system.js
+++ code/system.js
@@ -1,1 +1,26 @@
+var os = require('os');
+var fs = require('fs');
+
 // TODO: this file will contain system helpers.
+
+// Determines whether system functionality has been initialized.
+var initialized = false;
+
+// Ensures that the system-related functionality has been initialized.
+var init = exports.init = function init() {
+    if (initialized) return;
+    initializeTempStorage();
+    initialized = true;
+};
+
+// Sets up the temp storage if it's not there.
+function initializeTempStorage() {
+    var path = os.tmpdir() + '/' + tempStorageName();
+    fs.mkdirSync(path);
+}
+
+// Gets a name for the temp storage folder. This is a deterministic name,
+// made from the pid, such as 'avisynth.js-1234'.
+function tempStorageName() {
+    return 'avisynth.js-' + process.pid;
+}
