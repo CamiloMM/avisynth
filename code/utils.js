79becc72b4d2e022380b8dd2ef2cb0fa29da244e
CamiloMM <herzschaltung@yahoo.com.ar>
2014-12-12 14:04:45 -0200

    Directory removal utility.

--- code/utils.js
+++ code/utils.js
@@ -56,3 +56,23 @@ exports.ensureDirectory = function(path) {
         if (e.code != 'EEXIST') throw e;
     }
 };
+
+// Recursively remove a directory. Taken from https://gist.github.com/tkihira/2367067
+exports.removeDirectory = function(dir) {
+    var list = fs.readdirSync(dir);
+    for(var i = 0; i < list.length; i++) {
+        var filename = path.join(dir, list[i]);
+        var stat = fs.statSync(filename);
+        
+        if(filename == "." || filename == "..") {
+            // pass these files
+        } else if(stat.isDirectory()) {
+            // rmdir recursively
+            rmdir(filename);
+        } else {
+            // rm fiilename
+            fs.unlinkSync(filename);
+        }
+    }
+    fs.rmdirSync(dir);
+};
