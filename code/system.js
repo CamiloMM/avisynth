6d2cc9e085a7c6ffad7d19a6fa777bb106097e14
CamiloMM <herzschaltung@yahoo.com.ar>
2014-12-13 16:49:53 -0200

    Temo sub-path presence checker.

--- code/system.js
+++ code/system.js
@@ -12,21 +12,33 @@ var init = exports.init = function init() {
     initialized = true;
 };
 
-// Sets up the temp storage if it's not there.
-function initializeTempStorage() {
-    utils.ensureDirectory(tempStorageName());
-    utils.ensureDirectory(tempStorageName('scripts'));
+// Checks if a sub-path exists within the temporary directory for this session.
+// If it exists, it will return the absolute path for the sub-path given.
+// If it doesn't, it returns false.
+exports.temp = function(sub) {
+    var path = tempStoragePath(sub);
+    if (fs.existsSync(path)) {
+        return path;
+    } else {
+        return null;
+    }
 }
 
-// Gets a name for the temp storage folder. This is a deterministic name,
+// Sets up the temp storage if it's not there.
+function initializeTempStorage() {
+    utils.ensureDirectory(tempStoragePath());
+    utils.ensureDirectory(tempStoragePath('scripts'));
+}
+
+// Gets a path for the temp storage folder. This is a deterministic path,
 // made from the pid, such as '/tmp/avisynth.js-1234'.
 // Passing a "sub" parameter will return a sub-path like '/tmp/avisynth.js-1234/fooBar'.
-function tempStorageName(sub) {
+function tempStoragePath(sub) {
     var extra = sub ? '/' + sub : '';
     return os.tmpdir() + '/avisynth.js-' + process.pid + extra;
 }
 
 // Cleans up everything done by system features initialization.
 function cleanUp() {
-    utils.removeDirectory(tempStorageName());
+    utils.removeDirectory(tempStoragePath());
 }
