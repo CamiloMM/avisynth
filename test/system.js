f9e6bee36daa3312129a8784bedfa1d05ea45908
CamiloMM <herzschaltung@yahoo.com.ar>
2014-12-25 15:32:58 -0200

    .cleanUp tests.

--- test/system.js
+++ test/system.js
@@ -4,11 +4,19 @@ var os            = require('os');
 var system        = require('../code/system');
 
 describe('system-level features', function() {
-    it('should create a temporary directory', function() {
+    it('should create a temporary directory with .init()', function() {
         var tmp = path.resolve(os.tmpdir(), 'avisynth.js-' + process.pid);
         system.cleanUp();
         if (fs.existsSync(tmp)) throw new Error('"' + tmp + '" should not exist!');
         system.init();
         if (!fs.existsSync(tmp)) throw new Error('"' + tmp + '" should exist!');
+    });
+
+    it('should remove a temporary directory with .cleanUp()', function() {
+        var tmp = path.resolve(os.tmpdir(), 'avisynth.js-' + process.pid);
+        system.init();
+        if (!fs.existsSync(tmp)) throw new Error('"' + tmp + '" should exist!');
+        system.cleanUp();
+        if (fs.existsSync(tmp)) throw new Error('"' + tmp + '" should not exist!');
     });
 });
