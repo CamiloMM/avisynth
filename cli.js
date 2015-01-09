519c2fb7f7a2d11cc3b356622c16e0d92dc3770f
CamiloMM <herzschaltung@yahoo.com.ar>
2015-01-09 08:29:28 -0200

    Basis for the future command line interface.

---
+++ cli.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+// Command-line helper for avisynth.js - https://github.com/CamiloMM/avisynth
+
+var path = require('path');
+var my = require('./package.json');
+
+// The first argument is the command.
+// Currently, the second will always be a path to a script, if any.
+var name = path.basename(process.argv[1]);
+var command = process.argv[2];
+var script = process.argv[3];
+
+switch (command) {
+    case '-h':
+    case '--help':
+    case 'help':
+    case '/?': // ewww
+    case '':
+    case undefined:
+        showHelp();
+        break;
+    case '-v':
+    case '--version':
+    case 'version':
+        showVersion();
+        break;
+    default:
+        showHelp(command);
+}
+
+// Shows a help message.
+function showHelp(badArgument) {
+    showBanner();
+    if (badArgument !== undefined) {
+        console.log('Bad argument: "' + badArgument + '"');
+    }
+    console.log('Usage: ' + name + ' <command> [path]\n');
+    console.log('commands:');
+    console.log('    help    : show this help and exit.');
+    console.log('    version : show the version number and exit.');
+}
+
+// Shows the module's version.
+function showVersion() {
+    console.log(my.version);
+}
+
+// Shows the "banner".
+function showBanner() {
+    console.log(my.name + ' ' + my.version + ' by ' + my.author.name);
+    console.log(my.description);
+    console.log(my.homepage + '\n');
+};
